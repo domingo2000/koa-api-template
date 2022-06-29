6cffd0143035a5f559742031fade5bc9e04c1784
domingo2000 <domingo.edwards@uc.cl>
2022-06-28 22:48:11 -0400

    ft-test-framework-with-example

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import Koa from "koa";
+import koaBody from "koa-body";
+import logger from "koa-logger";
+import { router } from "./router";
+
+const app = new Koa();
+
+app.use(logger());
+
+// Middlewares
+app.use(
+  koaBody({
+    // The next configuration embrace security by only accepting JSON data
+    // multipart: false,
+    // urlencoded: false,
+    // text: false,
+    // json: true,
+  })
+);
+
+// Router configuration
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+export { app };
