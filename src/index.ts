377fbc28171237d20ef41d4f6847c4a068238faa
domingo2000 <domingo.edwards@uc.cl>
2022-06-28 21:50:40 -0400

    ft-prettier

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-import Koa from 'koa';
-import koaBody from 'koa-body';
-import logger from 'koa-logger';
-import {router} from './routes';
+import Koa from "koa";
+import koaBody from "koa-body";
+import logger from "koa-logger";
+import { router } from "./routes";
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,19 +10,20 @@ const app = new Koa();
 app.use(logger());
 
 // Middlewares
-app.use(koaBody({
-  // The next configuration embrace security by only accepting JSON data
-  // multipart: false,
-  // urlencoded: false,
-  // text: false,
-  // json: true,
-}));
+app.use(
+  koaBody({
+    // The next configuration embrace security by only accepting JSON data
+    // multipart: false,
+    // urlencoded: false,
+    // text: false,
+    // json: true,
+  })
+);
 
 // Router configuration
 app.use(router.routes());
 app.use(router.allowedMethods());
 
-
 app.listen(PORT, () => {
-  console.log('App listening in port', PORT);
+  console.log("App listening in port", PORT);
 });
